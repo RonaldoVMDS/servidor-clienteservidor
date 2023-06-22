cb2051290a1951182297ee4eeb0a7afa63aa0d33
Ronaldo <ronaldovinicius.machado@hotmail.com>
2023-06-22 12:56:14 -0300

    Teste entrega parcial 3

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => ['apiJwt']], function () {
     Route::get('users/{id}', [UserController::class, 'show']);
     Route::put('users/{id}', [UserController::class, 'update']);
-    Route::delete('users/{id}', [UserController::class, 'destroy']); // Verificar problemas com foreign key
+    Route::delete('users/{id}', [UserController::class, 'destroy']);
     Route::post('/occurrences', [OccurrenceController::class, 'createOccurrence']);
     Route::get('occurrences/users/{id}', [OccurrenceController::class, 'getUserOccurrences']);
     Route::put('occurrences/{occurrenceId}', [OccurrenceController::class, 'updateOccurrence']);
